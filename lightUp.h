ef59dd037cedb1448ff1b096b4bc4f9afe942343
NathanNormand <115269873+NathanNormand@users.noreply.github.com>
2022-10-08 18:21:04 -0700

    made code tidier

--- lightUp.h
+++ lightUp.h
@@ -1,14 +1,26 @@
 #include "Arduino.h"
 #include  "FastLED.h"
+#define NUM_LEDS 64       // total number of neopixels
+#define DATA_PIN 6        // pin for neopixels
 
-void lightUp(CRGB leds[], int NUM_LEDS, int m)
+CRGB leds[NUM_LEDS]; // define neopixel array
+int hue = 0;
+void setLights()
+{
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  FastLED.setBrightness(100);
+  FastLED.setMaxPowerInMilliWatts(1000);
+}
+
+void lightUp(int m) // these patterns will be changed for final install
 {
     switch(m)
   {
     case(0):
     
-        fill_rainbow(leds, NUM_LEDS, 0, 7);
-        
+        fill_rainbow(leds, NUM_LEDS, hue, 7);
+        hue+= 5;
+        if(hue > 255) hue = 0;
         FastLED.show();
         //fadeToBlackBy(leds, NUM_LEDS, 50);
     break;
@@ -30,7 +42,7 @@ void lightUp(CRGB leds[], int NUM_LEDS, int m)
   }
 }
 
-void lightOFF(CRGB leds[], int NUM_LEDS)
+void lightOFF()
 {
     fill_solid(leds,NUM_LEDS,CRGB::Black);
 }
